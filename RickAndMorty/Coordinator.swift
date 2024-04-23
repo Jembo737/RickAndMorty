bb1aa3aa48740104d9a636e9c935cc2dfb3c0798
Jembo737 <dz.saliya@gmail.com>
2024-04-23 13:07:14 +0400

    Delete main StoryBoard. Create coordinator

--- RickAndMorty/Coordinator.swift
+++ RickAndMorty/Coordinator.swift
@@ -5,4 +5,25 @@
 //  Created by Jemo on 23.04.24.
 //
 
-import Foundation
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get }
+    
+    func startCoordinator()
+}
+
+class MainCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = []
+    
+    var navigationController: UINavigationController = UINavigationController()
+    
+    func startCoordinator() {
+        let initialViewController = ViewController()
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.pushViewController(initialViewController, animated: false)
+    }
+    
+    
+}
